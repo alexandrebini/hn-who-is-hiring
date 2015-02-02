dd11d02f99577d63cd9688ab1015f56d85ceeaba
Alexandre Bini <alexandrebini@gmail.com>
2015-02-02 08:56:14 -0200

    all jobs

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,9 +1,12 @@
 class Api::JobsController < Api::BaseController
   def index
-    per_page = params[:per_page].to_i || 22
-    @jobs = Job.recent.page(params[:page]).per_page(per_page)
-    @pagination = { page: @jobs.current_page, per_page: @jobs.per_page,
-      total_pages: @jobs.total_pages, total_entries: @jobs.count }
-    render json: [@pagination, @jobs]
+    # per_page = params[:per_page].to_i || 22
+    @post = Post.recent.first
+    @jobs = @post.jobs.recent
+    # @jobs = @post.jobs.recent.page(params[:page]).per_page(per_page)
+    # @pagination = { page: @jobs.current_page, per_page: @jobs.per_page,
+    #   total_pages: @jobs.total_pages, total_entries: @jobs.count }
+    # render json: [@pagination, @jobs]
+    render json: @jobs
   end
 end
